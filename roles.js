ba3ec4cc09496d332af8c1481d4fe685e7b2d829
gat41 <gloria.tengaihui@gmail.com>
2016-12-01 01:03:08 +0800

    remove unneeded css styling

--- roles.js
+++ roles.js
@@ -1,11 +1,3 @@
-//2. certain user event on an emotion and let it react accordingly
-// - hover over Joy - change color or let it shake or omsething 
-// - click on sadness - let it turn blue and cry
-// - click on disgust - green
-// - click on anger - smoke comes out :D
-// - hover over fear - let it shake?
-
-
 function reactRole(){
 	joy = document.getElementById('joy');
 	joy.src = 'img/joy-mouse-over.jpg';
