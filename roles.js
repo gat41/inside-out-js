9b2b879038e4dd1fa2065b6966924d3d5423af12
gat41 <gloria.tengaihui@gmail.com>
2016-12-01 00:54:38 +0800

    complete reaction for joy

--- roles.js
+++ roles.js
@@ -4,3 +4,23 @@
 // - click on disgust - green
 // - click on anger - smoke comes out :D
 // - hover over fear - let it shake?
+
+
+function reactRole(){
+	joy = document.getElementById('joy');
+	joy.src = 'img/joy-mouse-over.jpg';
+}
+
+function resetRole(){
+	joy = document.getElementById('joy');
+	joy.src = 'img/joy.png';
+}
+
+function addRoleListener(){
+  joy = document.getElementById('joy');
+  joy.addEventListener("click", reactRole);
+  joy.addEventListener("dblclick", resetRole);
+}
+
+// wait for DOM content to load before binding an event
+document.addEventListener('DOMContentLoaded', addRoleListener);
